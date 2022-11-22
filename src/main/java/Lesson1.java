23afa5c1f98a413c8af28bb430abd55cc28ddf60
Valeentin87 <valeentin87@list.ru>
2022-11-22 17:22:33 +0300

    разбираемся с примитивными типами

--- src/main/java/Lesson1.java
+++ src/main/java/Lesson1.java
@@ -1,5 +1,6 @@
 public class Lesson1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) {  // точка входа в программму (сокращения main, либо pvsm)
+
 
     }
 }
