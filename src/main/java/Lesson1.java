6c17fa6caefe1450097d08a6ee5c345f7df46c8f
Valeentin87 <valeentin87@list.ru>
2022-11-22 22:54:50 +0300

    финальное сохранение семинар 1 java

--- src/main/java/Lesson1.java
+++ src/main/java/Lesson1.java
@@ -1,6 +1,11 @@
 public class Lesson1 {
     public static void main(String[] args) {  // точка входа в программму (сокращения main, либо pvsm)
 
+    Integer.bitCount(int i:56);
+    Integer.numberOfLeadingZeros();
+    Integer.numberOfTrailingZeros();
+    Integer.toHexString();
+    Integer.toOctalString();
 
     }
 }
